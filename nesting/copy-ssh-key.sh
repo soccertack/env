f45f8adb7e1c1e6f4e2d18ba5f24284ec336be76
Jintack Lim <jintack@cs.columbia.edu>
2017-08-31 11:15:05 -0600

    Copy ssh keys to L0, L1 and L2
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

---
+++ nesting/copy-ssh-key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HOST_KEYS=~/.ssh/authorized_keys
+GUEST_KEYS=/root/.ssh/authorized_keys
+
+echo $1 >> $HOST_KEYS
+ssh root@10.10.1.100 "echo $1 >> $GUEST_KEYS"
+ssh root@10.10.1.101 "echo $1 >> $GUEST_KEYS"
