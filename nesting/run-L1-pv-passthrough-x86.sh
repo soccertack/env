210bdd5859971ccb6852137668f383a1f438e039
Jintack Lim <jintack@cs.columbia.edu>
2018-01-22 21:30:55 -0700

    Add qmp for L1-pv-passthrough-x86
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/run-L1-pv-passthrough-x86.sh
+++ nesting/run-L1-pv-passthrough-x86.sh
@@ -3,4 +3,5 @@ qemu-system-x86_64 -M q35,accel=kvm -m 12G \
 		-netdev user,id=net0,hostfwd=tcp::2222-:22 \
 		-device virtio-net-pci,netdev=net0 \
 		-device vfio-pci,host=02:00.0,id=net1 \
+		-qmp unix:/var/run/qmp,server,nowait \
 		-drive file=/vm/l2guest.img,format=raw --nographic
