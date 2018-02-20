f89daccdb3bad13b6d8de42756ebc14094fdfefd
Jintack Lim <jintack@cs.columbia.edu>
2018-02-20 07:47:58 -0700

    Use macro for nested.sh
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nested.sh
+++ nested.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-cp /proj/kvmarm-PG0/jintack/nested/v4.15.img.bz2 /vmdata/
-pushd /vmdata;pbzip2 -dk v4.15.img.bz2 && mv v4.15.img linaro-trusty.img;popd
+SRC_BZ=/proj/kvmarm-PG0/jintack/nested/v4.15.img.bz2
+BZ=`echo ${SRC_BZ##*/}`
+IMG=`echo ${BZ%.*}`
+TARGET_IMG=linaro-trusty.img
+IMG_DIR=/vmdata
 
-mount -o loop /vmdata/linaro-trusty.img /mnt
-cat /users/jintackl/.ssh/id_rsa.pub >> /mnt/root/.ssh/authorized_keys
+cp $SRC_BZ $IMG_DIR
+pushd $IMG_DIR
+pbzip2 -dk $BZ
+mv $IMG $TARGET_IMG
+popd
+
+sudo mount -o loop $IMG_DIR/$TARGET_IMG /mnt
+cat $HOME/.ssh/id_rsa.pub | sudo tee -a /mnt/root/.ssh/authorized_keys
 mkdir -p /mnt_l2
-mount -o loop /mnt/root/vm/l2.img /mnt_l2
-cat /users/jintackl/.ssh/id_rsa.pub >> /mnt_l2/root/.ssh/authorized_keys
-umount /mnt_l2
-umount /mnt
+sudo mount -o loop /mnt/root/vm/l2.img /mnt_l2
+cat $HOME/.ssh/id_rsa.pub | sudo tee -a /mnt_l2/root/.ssh/authorized_keys
+sudo umount /mnt_l2
+sudo umount /mnt
 
-cp nesting/run.sh /users/jintackl
-cp nesting/trap_count.sh /users/jintackl
-cp nesting/consume_mem.sh /users/jintackl
-cp nesting/kvm_trace.sh /users/jintackl
-cp nesting/copy-ssh-key.sh /users/jintackl
-cp nesting/pin_vcpus_all.sh /users/jintackl
-cp nesting/micro-cycles.py /users/jintackl
-
+cp nesting/run.sh $HOME
+cp nesting/trap_count.sh $HOME
+cp nesting/consume_mem.sh $HOME
+cp nesting/kvm_trace.sh $HOME
+cp nesting/copy-ssh-key.sh $HOME
+cp nesting/pin_vcpus_all.sh $HOME
+cp nesting/micro-cycles.py $HOME
