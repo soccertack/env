a71f7b59edb6070af7eb699055584c8cf4fc98cf
Jintack Lim <jintack@cs.columbia.edu>
2018-02-20 08:55:14 -0600

    Create a partition in SDD :)
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

---
+++ nesting/mkfs-wisc-sdc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SDC=`sudo fdisk -l 2>/dev/null | grep "480103981056 bytes" | awk '{print $2}' | cut -d: -f1 | cut -d/ -f3`
+
+if [[ $SDC != "sdc" ]]; then
+	echo "Check if you have an SSD disk at /dev/sdc"
+	exit
+fi
+
+if [[ ! -e /dev/sdc1 ]]; then
+	parted -s -a optimal /dev/sdc -- mklabel msdos mkpart primary ext4 1 -1
+fi
+
+mount | grep /dev/sdc1 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+	mkfs.ext4 /dev/sdc1
+	mkdir -p /vm2
+	mount /dev/sdc1 /vm2
+fi
