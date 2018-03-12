3e23793d2d3d38f25ccd82d111e117a17e494ad9
Jintack Lim <jintack@cs.columbia.edu>
2018-03-11 21:24:50 -0500

    Slighly update update-l2 script
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/update-l2-remote.sh
+++ nesting/update-l2-remote.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+#Check if qemu is running
+pgrep qemu
+err=$?
+if [[ $err == 0 ]]; then
+	echo "QEMU is running. Please terminate VM(s) to update L2 VM kernel"
+	exit 1
+fi
+
 source ~/.cloudlab_ip.sh
 
 VM=/vmdata
 KVM_L2=.
 md5sum $KVM_L2/arch/arm64/boot/Image
 echo $IP
-scp $KVM_L2/arch/arm64/boot/Image jintackl@$IP:~/L2_Image
+
+scp $KVM_L2/arch/arm64/boot/Image jintackl@$IP:/tmp
 ssh jintackl@$IP "sudo mount -o loop $VM/linaro-trusty.img /mnt"
-ssh jintackl@$IP "sudo cp ~/L2_Image  /mnt/root/vm/Image"
+ssh jintackl@$IP "sudo cp /tmp  /mnt/root/vm/Image"
 ssh jintackl@$IP "sudo umount /mnt"
 ssh jintackl@$IP "sync"
+
