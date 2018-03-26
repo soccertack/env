4463225fb91bd20d8eaf170947cd6759a8517998
Jintack Lim <jintack@cs.columbia.edu>
2018-03-26 10:42:42 -0500

    Update the host kernel image only if it is out-of-date
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/update-host.sh
+++ nesting/update-host.sh
@@ -4,6 +4,7 @@ UIMAGE=arch/arm64/boot/uImage
 IMAGE=arch/arm64/boot/Image
 OUT=.
 
+# Create uImage file only if it is out-of date
 if [ "$OUT/$UIMAGE" -ot "$OUT/$IMAGE" ]; then
 	echo "Generate uImage..."
 	mkimage -A arm -O linux -C none -T kernel -a 0x00080000 -e 0x00080000 -n Linux \
@@ -11,9 +12,19 @@ if [ "$OUT/$UIMAGE" -ot "$OUT/$IMAGE" ]; then
 fi
 
 source ~/.cloudlab_ip.sh
-echo $IP
 
-md5sum $OUT/arch/arm64/boot/uImage
+MD5_LOCAL="`md5sum ${OUT}/arch/arm64/boot/uImage | awk '{print $1}'`"
+
+MD5_CMD="ssh jintackl@${IP} md5sum /boot/uImage-columbia | awk '{print \$1}'"
+MD5_SERV="`eval ${MD5_CMD}`"
+
+# Update uImage file only if it is out-of date
+if [ "$MD5_SERV" == "$MD5_LOCAL" ]; then
+	echo "The server uImage is up-to-date."
+	echo $MD5_SERV
+	exit
+fi
+
 scp $OUT/arch/arm64/boot/uImage jintackl@$IP:~
 ssh jintackl@$IP "sudo cp ~/uImage /boot/uImage-columbia"
 ssh jintackl@$IP "sync"
