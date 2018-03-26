ffe0ad1d89492f8697c4107eba53b5d76a454644
Jintack Lim <jintack@cs.columbia.edu>
2018-03-26 10:20:33 -0500

    Create uImage only when Image is updated
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/update-host.sh
+++ nesting/update-host.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+UIMAGE=arch/arm64/boot/uImage
+IMAGE=arch/arm64/boot/Image
+OUT=.
+
+if [ "$OUT/$UIMAGE" -ot "$OUT/$IMAGE" ]; then
+	echo "Generate uImage..."
+	mkimage -A arm -O linux -C none -T kernel -a 0x00080000 -e 0x00080000 -n Linux \
+	-d $OUT/arch/arm64/boot/Image $OUT/arch/arm64/boot/uImage
+fi
+
 source ~/.cloudlab_ip.sh
 echo $IP
-
-OUT=.
-mkimage -A arm -O linux -C none -T kernel -a 0x00080000 -e 0x00080000 -n Linux \
-	-d $OUT/arch/arm64/boot/Image \
-		$OUT/arch/arm64/boot/uImage
 
 md5sum $OUT/arch/arm64/boot/uImage
 scp $OUT/arch/arm64/boot/uImage jintackl@$IP:~
