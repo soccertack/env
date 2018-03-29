29f3ccacc42561bbcc5fcf8e9545b38002523343
Jintack Lim <jintack@cs.columbia.edu>
2018-03-29 15:08:09 -0500

    Rsync modules correctly to the VM
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/rsync_kernel_arm.sh
+++ nesting/rsync_kernel_arm.sh
@@ -4,9 +4,9 @@ source ~/.cloudlab_ip.sh
 echo "----- rsync start ----"
 echo $IP
 
-KERNEL_VER="4.15.0+"
+KERNEL_VER="4.15.0"
 INSTALL_MOD_PATH="my_modules"
 
 time rsync -av $INSTALL_MOD_PATH/lib/modules/$KERNEL_VER jintackl@$IP:~
-ssh jintackl@$IP "rsync -av $KERNEL_VER/ root@10.10.1.100:/lib/modules/"
+ssh jintackl@$IP "rsync -av $KERNEL_VER root@10.10.1.100:/lib/modules/"
 echo "----- rsync done ----"
