36f1895669b5ceba5f734f3abaf689009a45bbb1
Jintack Lim <jintack@cs.columbia.edu>
2018-04-07 22:24:02 -0500

    Update guest Image only when it is changed
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/update-guest.sh
+++ nesting/update-guest.sh
@@ -4,7 +4,18 @@ source ~/.cloudlab_ip.sh
 echo $IP
 
 OUT=.
-md5sum $OUT/arch/arm64/boot/Image
-scp $OUT/arch/arm64/boot/Image jintackl@$IP:~
-ssh jintackl@$IP "sudo cp ~/Image /srv/vm"
+
+MD5_CMD="ssh jintackl@${IP} md5sum /srv/vm/Image | awk '{print \$1}'"
+MD5_SERV="`eval ${MD5_CMD}`"
+MD5_LOCAL="`md5sum $OUT/arch/arm64/boot/Image | awk '{print $1}'`"
+
+# Update Image file only if it is out-of date
+if [ "$MD5_SERV" == "$MD5_LOCAL" ]; then
+	echo "The server Image is up-to-date."
+	echo $MD5_SERV
+	exit
+fi
+
+scp $OUT/arch/arm64/boot/Image jintackl@$IP:/tmp
+ssh jintackl@$IP "sudo cp /tmp/Image /srv/vm"
 ssh jintackl@$IP "sync"
