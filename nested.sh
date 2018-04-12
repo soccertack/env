20bf970859fb13cba315aa68ec29f6686d97575c
Jintack Lim <jintack@cs.columbia.edu>
2018-04-12 13:20:14 -0500

    untar x86 image file
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nested.sh
+++ nested.sh
@@ -2,12 +2,19 @@
 
 HOME=${1:-$HOME}
 
+ARCH=`uname -m`
+
 SRC_BZ=/proj/kvmarm-PG0/jintack/nested/v4.15.img.bz2
 BZ=`echo ${SRC_BZ##*/}`
 IMG=`echo ${BZ%.*}`
-TARGET_IMG=linaro-trusty.img
-IMG_DIR=/vmdata
 SCRIPT_DIR=nesting
+if [[ "$ARCH" == "x86_64" ]]; then
+	TARGET_IMG=guest0.img
+	IMG_DIR=/vm
+else
+	TARGET_IMG=linaro-trusty.img
+	IMG_DIR=/vmdata
+fi
 
 cp $SRC_BZ $IMG_DIR
 pushd $IMG_DIR
@@ -15,13 +22,15 @@ pbzip2 -kd $BZ
 mv $IMG $TARGET_IMG
 popd
 
-sudo mount -o loop $IMG_DIR/$TARGET_IMG /mnt
-cat $HOME/.ssh/id_rsa.pub | sudo tee -a /mnt/root/.ssh/authorized_keys
-mkdir -p /mnt_l2
-sudo mount -o loop /mnt/root/vm/l2.img /mnt_l2
-cat $HOME/.ssh/id_rsa.pub | sudo tee -a /mnt_l2/root/.ssh/authorized_keys
-sudo umount /mnt_l2
-sudo umount /mnt
+if [[ "$ARCH" == "aarch64" ]]; then
+	sudo mount -o loop $IMG_DIR/$TARGET_IMG /mnt
+	cat $HOME/.ssh/id_rsa.pub | sudo tee -a /mnt/root/.ssh/authorized_keys
+	mkdir -p /mnt_l2
+	sudo mount -o loop /mnt/root/vm/l2.img /mnt_l2
+	cat $HOME/.ssh/id_rsa.pub | sudo tee -a /mnt_l2/root/.ssh/authorized_keys
+	sudo umount /mnt_l2
+	sudo umount /mnt
+fi
 
 pushd $SCRIPT_DIR
 HOME_LIST="run.sh trap_count.sh pin_vcpus_all.sh"
