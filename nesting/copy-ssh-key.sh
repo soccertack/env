88f6ef8664429666d24131258b1d8bff811adb51
Jintack Lim <jintack@cs.columbia.edu>
2018-04-12 23:06:15 -0500

    Add ssh key to L3 VM
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/copy-ssh-key.sh
+++ nesting/copy-ssh-key.sh
@@ -6,3 +6,4 @@ GUEST_KEYS=/root/.ssh/authorized_keys
 echo $1 >> $HOST_KEYS
 ssh root@10.10.1.100 "echo $1 >> $GUEST_KEYS"
 ssh root@10.10.1.101 "echo $1 >> $GUEST_KEYS"
+ssh root@10.10.1.102 "echo $1 >> $GUEST_KEYS"
