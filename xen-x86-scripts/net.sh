c2a7dc27a7d19772ba17695af8cb6072970c828b
Jintack Lim <jintack@cs.columbia.edu>
2018-05-01 22:12:05 -0600

    Find a right eth
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- xen-x86-scripts/net.sh
+++ xen-x86-scripts/net.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+ETH=`ifconfig | grep "10\.10\." -B1 | head -n 1 | awk '{ print $1 }'`
+
 ./net_forward.sh
 
-ifconfig eth2 > /dev/null 2>&1
+ifconfig $ETH > /dev/null 2>&1
 err=$?
 if [[ $err != 0 ]]; then
 	echo "eth2 not found - are you using the right topology?" >&2
 	exit 1
 fi
 
-IP=`ifconfig eth2 | grep 'inet addr:' | awk '{ print $2 }' | sed 's/.*://'`
-ifconfig eth2 0.0.0.0
+IP=`ifconfig $ETH | grep 'inet addr:' | awk '{ print $2 }' | sed 's/.*://'`
+ifconfig $ETH 0.0.0.0
 brctl addbr xenbr1
-brctl addif xenbr1 eth2
+brctl addif xenbr1 $ETH
 ifconfig xenbr1 $IP netmask 255.255.255.0
 
 sed -i '/vif =/d' domU.conf
