ed55826af17e00b1cf333de13590d3472b7afea2
Jintack Lim <jintack@cs.columbia.edu>
2018-05-14 11:32:50 -0400

    Setup x86 nodes in Cloudlab - try again
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/setup-cloudlab.sh
+++ nesting/setup-cloudlab.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-ARCH=`uname -m`
 ENV_PATH=/tmp/env/nesting
 
-if [[ "$ARCH" == "aarch64" ]]; then
-
-        uname -a | grep -q kvm-node
-        err=$?
-        if [[ $err == 0 ]]; then
-                # kvm node
-                source /$ENV_PATH/setup-nested.sh
-        else
-                # client node
-                source /$ENV_PATH/setup-common.sh
-        fi
+uname -a | grep -q kvm-node
+err=$?
+if [[ $err == 0 ]]; then
+	# kvm node
+	source /$ENV_PATH/setup-nested.sh
+else
+	# client node
+	source /$ENV_PATH/setup-common.sh
 fi
 
 cat /$ENV_PATH/keys | sudo tee /root/.ssh/authorized_keys -a
