9a63fed4df7fad36d48c0e8f9882e8920e500768
Jintack Lim <jintack@cs.columbia.edu>
2018-06-29 13:25:49 -0500

    Set the number of cpus
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/kexec-maxcpus.sh
+++ nesting/kexec-maxcpus.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+read -p "maxcpus=" MAX_CPUS
+
 CMD_LINE=`cat /proc/cmdline`
 echo $CMD_LINE
-CMD_LINE=`cat /proc/cmdline | sed "s/maxcpus=[0-9]*/maxcpus=4/"`
+CMD_LINE=`cat /proc/cmdline | sed "s/maxcpus=[0-9]*/maxcpus=$MAX_CPUS/"`
 echo $CMD_LINE
 
 kexec -l /boot/vmlinuz-4.15.0-kexec --initrd=/boot/initrd.img-4.15.0-kexec --command-line="$CMD_LINE"
