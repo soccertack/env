71b90a15f1099aa9c4cdfd083605021ca4a75747
Jintack Lim <jintack@cs.columbia.edu>
2018-06-29 13:20:54 -0500

    Do copy scripts both on kvm and other nodes
    Copy kexec-maxcpus.sh, too
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/setup-cloudlab.sh
+++ nesting/setup-cloudlab.sh
@@ -12,4 +12,11 @@ else
 	source /$ENV_PATH/setup-common.sh
 fi
 
+SCRIPT_DIR=nesting
+USR_BIN=/usr/local/bin
+pushd /tmp/env/$SCRIPT_DIR
+BIN_LIST="build-n-install.sh copy-kernel.sh kexec-maxcpus.sh"
+sudo cp $BIN_LIST $USR_BIN
+popd
+
 cat /$ENV_PATH/keys | sudo tee /root/.ssh/authorized_keys -a
--- nesting/setup-common.sh
+++ nesting/setup-common.sh
@@ -6,9 +6,3 @@ sudo ./env.py -f -u root -a
 sudo ./env.py -f -u jintackl -a
 popd
 
-SCRIPT_DIR=nesting
-USR_BIN=/usr/local/bin
-pushd /tmp/env/$SCRIPT_DIR
-BIN_LIST="build-n-install.sh copy-kernel.sh"
-sudo cp $BIN_LIST $USR_BIN
-popd
