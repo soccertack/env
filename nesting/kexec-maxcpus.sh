69023a1a86177783731c860fed1b50402bd16fac
Jintack Lim <jintack@cs.columbia.edu>
2018-07-10 18:07:08 -0500

    Try to make kexec script better - WIP
    TODO: ask nested option, intel_iommu option, nohz option, kernel
    version, maxcpus
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/kexec-maxcpus.sh
+++ nesting/kexec-maxcpus.sh
@@ -7,6 +7,9 @@ echo $CMD_LINE
 CMD_LINE=`cat /proc/cmdline | sed "s/maxcpus=[0-9]*/maxcpus=$MAX_CPUS/"`
 echo $CMD_LINE
 
-kexec -l /boot/vmlinuz-4.15.0-kexec --initrd=/boot/initrd.img-4.15.0-kexec --command-line="$CMD_LINE"
+#CMD_LINE="$CMD_LINE kvm-intel.nested=1"
+#VER=-exits
+
+kexec -l /boot/vmlinuz-4.15.0$VER --initrd=/boot/initrd.img-4.15.0$VER --command-line="$CMD_LINE"
 
 kexec -e
