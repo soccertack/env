e222e4c246be2316f96dafa2067052293e4970be
Jintack Lim <jintack@cs.columbia.edu>
2018-08-29 18:45:54 -0500

    Put client ssh key to a shared dir
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/setup-common.sh
+++ nesting/setup-common.sh
@@ -6,3 +6,8 @@ sudo ./env.py -f -u root -a
 sudo ./env.py -f -u jintackl -a
 popd
 
+EXP_NAME=`uname -a | awk '{print $2}' | cut -d. -f2`
+
+KEY_DIR=/proj/kvmarm-PG0/jintack/keys/
+mkdir $KEY_DIR/$EXP_NAME/
+cp /users/jintack/.ssh/id_rsa.pub $KEY_DIR/$EXP_NAME/client-key
