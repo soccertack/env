7044eeaca2180a2a5b75d8c5be00dffd4fb5bf1e
Jintack Lim <jintack@cs.columbia.edu>
2019-01-04 21:08:07 -0600

    Mount /vm in the nfs client
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/setup-nfs-client.sh
+++ nesting/setup-nfs-client.sh
@@ -13,7 +13,11 @@ NFS_DIR=${2:-$DEFAULT_DIR}
 DEFAULT_DIR=/sdc
 NFS_MOUNT_DIR=${3:-$DEFAULT_DIR}
 
+NFS_VM_DIR=/vm
+NFS_VM_MOUNT_DIR=/vm_nfs
+
 mkdir -p $NFS_MOUNT_DIR
+mkdir -p $NFS_VM_MOUNT_DIR
 
 dpkg -l | grep -q nfs-common
 if [ $? != 0 ]; then
@@ -21,4 +25,5 @@ if [ $? != 0 ]; then
 	sudo apt-get install nfs-common
 fi
 mount $NFS_IP:$NFS_DIR $NFS_MOUNT_DIR
+mount $NFS_IP:$NFS_VM_DIR $NFS_VM_MOUNT_DIR
 df -h
