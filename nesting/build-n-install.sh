01bb5a5aeb2b26090760b38580673bb3265ae5b7
Jintack Lim <jintack@cs.columbia.edu>
2019-01-25 09:50:00 -0600

    Update build-n-install
    - Get LV from the branch name
    - Skip modules install optionally
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/build-n-install.sh
+++ nesting/build-n-install.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+branch_name=`git symbolic-ref --short HEAD`
+LV_DEFAULT=-`echo $branch_name | cut -d"-" -f2-`
 
 if [ -z "$1" ]; then
-	read -p "LOCALVERSION? (Don't forget to put a dash):" LV
+	read -p "LOCALVERSION?[$LV_DEFAULT]:" LV
+	if [ "$LV" == "" ]; then
+		LV=$LV_DEFAULT
+	fi
 else
 	LV="$1"
 fi
 
-time make -j 40 LOCALVERSION=$LV && sudo make modules_install && sudo make install
+MOD_INSTALL="sudo make modules_install"
+if [ -z "$2" ]; then
+	echo "Install modules and kernel"
+else
+	# dummy command
+	MOD_INSTALL="ls"
+fi
+
+time make -j 40 LOCALVERSION=$LV && $MOD_INSTALL && sudo make install
