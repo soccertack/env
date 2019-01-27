a54719052f42d8eb4c2346c316de151a25b8bfa8
Jintack Lim <jintack@cs.columbia.edu>
2019-01-27 09:41:16 -0600

    kexec-kernel test
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

---
+++ nesting/kexec-kernel.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CURR=`uname -r`
+read -p "kernel:[$CURR]" VER
+
+if [ "$VER" = "" ]; then
+	VER="$CURR"
+
+CMD_LINE=`cat /proc/cmdline`
+
+kexec -l /boot/vmlinuz-$VER --initrd=/boot/initrd.img-$VER --command-line="$CMD_LINE"
+
+kexec -e
