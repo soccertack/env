bd7706dd0f3590a7cbf56c0358b989548a36a70e
Jintack Lim <jintack@cs.columbia.edu>
2019-02-10 16:10:02 -0600

    clean-up setup-common.h
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/setup-common.sh
+++ nesting/setup-common.sh
@@ -2,14 +2,8 @@
 
 ENV_DIR=/tmp/env
 
-KEY_DIR=/proj/kvmarm-PG0/jintack/keys/
-git clone -q https://github.com/soccertack/kvmperf.git /users/jintackl
 pushd $ENV_DIR
 sudo ./env.py -f -u root -a
 sudo -u jintackl -i -- sh -c 'cd /tmp/env ; /tmp/env/env.py -f -a'
 popd
 
-EXP_NAME=`uname -a | awk '{print $2}' | cut -d. -f2`
-
-sudo mkdir -p $KEY_DIR/$EXP_NAME/
-sudo cp /users/jintackl/.ssh/id_rsa.pub $KEY_DIR/$EXP_NAME/client-key
