77640d9288bfacebb6b0fb575f436bc6c5daa188
Jintack Lim <jintack@cs.columbia.edu>
2019-02-10 20:23:26 -0600

    Move common configurations to a common file
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- nesting/setup-cloudlab.sh
+++ nesting/setup-cloudlab.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 
-ENV_PATH=/tmp/env/nesting
-
-uname -a | grep -q kvm-
-err=$?
-if [[ $err == 0 ]]; then
-	# kvm node
-	source /$ENV_PATH/setup-nested.sh
-else
-	# client node
-	source /$ENV_PATH/setup-common.sh
-fi
-
-SCRIPT_DIR=nesting
-USR_BIN=/usr/local/bin
-pushd /tmp/env/$SCRIPT_DIR
-BIN_LIST="build-n-install.sh copy-kernel.sh kexec-maxcpus.sh kexec-kernel.sh"
-
-for f in $BIN_LIST; do
-	rm /usr/local/bin/$f
-	sudo ln -s /tmp/env/$SCRIPT_DIR/$f /usr/local/bin/$f
-done
-
-popd
-
-cat /$ENV_PATH/keys | sudo tee /root/.ssh/authorized_keys -a
+echo "hello"
--- nesting/setup-common.sh
+++ nesting/setup-common.sh
@@ -7,3 +7,15 @@ sudo ./env.py -f -u root -a
 sudo -u jintackl -i -- sh -c 'cd /tmp/env ; /tmp/env/env.py -f -a'
 popd
 
+SCRIPT_DIR=$ENV_DIR/scripts
+USR_BIN=/usr/local/bin
+BIN_LIST="build-n-install.sh copy-kernel.sh kexec-maxcpus.sh kexec-kernel.sh"
+
+pushd $SCRIPT_DIR
+for f in $BIN_LIST; do
+	rm /usr/local/bin/$f
+	sudo ln -s $SCRIPT_DIR/$f /usr/local/bin/$f
+done
+popd
+
+cat $SCRIPT_DIR/keys | sudo tee /root/.ssh/authorized_keys -a
