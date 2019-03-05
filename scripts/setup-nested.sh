914cc400d7d65b3efe53436045376aca6cfa662a
Jintack Lim <jintack@cs.columbia.edu>
2019-03-05 11:56:58 -0600

    Get the kernel version for guest image
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/setup-nested.sh
+++ scripts/setup-nested.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ARCH=`uname -m`
+K_VER=${1:-4.15}
 
 if [[ "$ARCH" == "x86_64" ]]; then
-	BZ_FILE="/vm/v4.15.img.bz2"
+	BZ_FILE="/vm/v${K_VER}.img.bz2"
 elif [[ "$ARCH" == "aarch64" ]]; then
-	BZ_FILE="/vmdata/v4.15.img.bz2"
+	BZ_FILE="/vmdata/v${K_VER}.img.bz2"
 fi
 
 # Quit if v4.15.img.bz2 exists
