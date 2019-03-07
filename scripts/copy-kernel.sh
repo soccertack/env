b2efda4833de58c987def5bb4485f499fb78f312
Jintack Lim <jintack@cs.columbia.edu>
2019-03-07 17:55:38 -0600

    fixup! handle node.exp format

--- scripts/copy-kernel.sh
+++ scripts/copy-kernel.sh
@@ -35,7 +35,6 @@ else
 	get_ip $TARGET_IP
 	echo "after conversion: $TARGET_IP"
 fi
-exit
 
 RELEASE_FILE="include/config/kernel.release"
 
@@ -48,8 +47,7 @@ else
 	echo $KERNEL_VER
 fi
 
-TIME_FORMAT="\n%E real\n%U user\n%S sys\n"
-time -f "$TIME_FORMAT" rsync -av /lib/modules/$KERNEL_VER root@$TARGET_IP:/lib/modules/.
-time -f "$TIME_FORMAT" scp /boot/*$KERNEL_VER root@$TARGET_IP:/boot/.
+rsync -av /lib/modules/$KERNEL_VER root@$TARGET_IP:/lib/modules/.
+scp /boot/*$KERNEL_VER root@$TARGET_IP:/boot/.
 
 echo "Don't forget to update /etc/default/grub to use a new kernel"
