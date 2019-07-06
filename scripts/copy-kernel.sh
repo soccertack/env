41a02dcee67053c638e88d020b246a6b276dd1b8
Jintack Lim <jintack@cs.columbia.edu>
2019-07-05 19:10:28 -0500

    Take shortcuts for vm IP addresses
    While we are at it, change the default kernel version to the one in the
    destination
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/copy-kernel.sh
+++ scripts/copy-kernel.sh
@@ -31,6 +31,10 @@ fi
 
 if expr "$TARGET_IP" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
 	echo "success"
+elif [ "$TARGET_IP" == "L1" ] || [ "$TARGET_IP" == "l1" ]; then
+	TARGET_IP="10.10.1.100"
+elif [ "$TARGET_IP" == "L2" ] || [ "$TARGET_IP" == "l2" ]; then
+	TARGET_IP="10.10.1.101"
 else
 	get_ip $TARGET_IP
 	echo "after conversion: $TARGET_IP"
@@ -43,7 +47,7 @@ if [ -f $RELEASE_FILE ]; then
 elif [ ! -z  $2 ]; then
 	KERNEL_VER=$2
 else
-	DEFAULT_KERNEL=`uname -r`
+	DEFAULT_KERNEL=`ssh root@$TARGET_IP uname -r`
 	read -p "Kernel version[$DEFAULT_KERNEL]: " ver
 	KERNEL_VER=${ver:-$DEFAULT_KERNEL}
 	echo $KERNEL_VER
