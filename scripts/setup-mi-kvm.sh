faf1c8cc9df4dcad759f86a20f3d27a1ee6c02a8
Jintack Lim <jintack@cs.columbia.edu>
2019-08-23 16:07:48 -0500

    Copy client key to the image used for migration
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/setup-mi-kvm.sh
+++ scripts/setup-mi-kvm.sh
@@ -6,6 +6,9 @@ pushd /srv/vm
 ./remove-nested-py
 popd
 
+# Add client key to the image to be copied first
+./mount-and-copy-ssh-key.sh
+
 mount -o loop,offset=1048576 /vm/guest0.img /mnt_l1
 cp /mnt_l1/vm/guest0.img /vm_nfs/
 umount /mnt_l1
