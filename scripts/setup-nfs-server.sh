541231cf871f73b5f41173b7fa85c8f594bb2c93
Jintack Lim <jintack@cs.columbia.edu>
2019-09-03 14:10:30 -0500

    Make setup-nfs-server to use /vm as a default dir
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/setup-nfs-server.sh
+++ scripts/setup-nfs-server.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-NFS_DIR=${1:-/sdc}
 VM_DIR=/vm
-
-DEFAULT_DIR=/sdc
+DEFAULT_DIR=/vm
 read -p "nfs source dir[$DEFAULT_DIR]: " dir
 NFS_DIR=${dir:-$DEFAULT_DIR}
 echo $NFS_VER
 
-if [ "$NFS_DIR" == "$DEFAULT_DIR" ]; then
-	read -p "Want to create file system at $DEFAULT_DIR?[y/n]: " yesno
+if [ "$NFS_DIR" == "\sdc" ]; then
+	read -p "Want to create file system at $NFS_DIR?[y/n]: " yesno
 	if [ $yesno == "y" ]; then
 		source mkfs-wisc-sdc.sh
 	fi
@@ -18,6 +16,5 @@ fi
 apt-get update
 apt-get -y install nfs-kernel-server
 echo "$NFS_DIR *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "$VM_DIR *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 exportfs -a
 service nfs-kernel-server start
