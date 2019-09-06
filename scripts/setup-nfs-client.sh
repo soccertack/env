52e89ffb1d5eb99899abc0f8587a852811f1bc84
Jintack Lim <jintack@cs.columbia.edu>
2019-09-06 16:41:32 -0500

    Forget about platform dependent dir setting
    We will always use /vm in the client
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/setup-nfs-client.sh
+++ scripts/setup-nfs-client.sh
@@ -7,16 +7,9 @@
 DEFAULT_IP="10.10.1.1"
 NFS_IP=${1:-$DEFAULT_IP}
 
-DEFAULT_DIR=/sdc
-NFS_DIR=${2:-$DEFAULT_DIR}
-
-DEFAULT_DIR=/sdc
-NFS_MOUNT_DIR=${3:-$DEFAULT_DIR}
-
 NFS_VM_DIR=/vm
 NFS_VM_MOUNT_DIR=/vm_nfs
 
-mkdir -p $NFS_MOUNT_DIR
 mkdir -p $NFS_VM_MOUNT_DIR
 
 dpkg -l | grep -q nfs-common
@@ -24,6 +17,5 @@ if [ $? != 0 ]; then
 	sudo apt-get update
 	sudo apt-get install nfs-common
 fi
-mount $NFS_IP:$NFS_DIR $NFS_MOUNT_DIR
 mount $NFS_IP:$NFS_VM_DIR $NFS_VM_MOUNT_DIR
 df -h
