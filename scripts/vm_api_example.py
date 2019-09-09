b1a49c0836b1580def28de71d711af9309bb0b2b
Jintack Lim <jintack@cs.columbia.edu>
2019-09-09 12:05:51 -0500

    Update vm_api exmple
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/vm_api_example.py
+++ scripts/vm_api_example.py
@@ -3,8 +3,7 @@
 import vm_api
 import time
 
-vm_api.init()
+vm_api.init(False)
 vm_api.boot_vms()
-print("Hi we are here")
-time.sleep(5)
-vm_api.halt(2)
+child = vm_api.get_child()
+child.interact()
