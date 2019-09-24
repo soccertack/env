1ad78bf0329c619c24e53419d358a9ebc4da2ece
Jintack Lim <jintack@cs.columbia.edu>
2019-09-24 16:41:06 -0700

    Remove unnecessary dbg messages
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/setup-nested.sh
+++ scripts/setup-nested.sh
@@ -3,14 +3,6 @@
 ARCH=`uname -m`
 K_VER=${1:-4.15}
 
-echo $1
-TMP_FILE=/tmp/env/ver
-sudo touch $TMP_FILE
-echo "first param" | sudo tee --append $TMP_FILE
-echo $1 | sudo tee --append $TMP_FILE
-echo "kver" | sudo tee --append $TMP_FILE
-echo $K_VER | sudo tee --append $TMP_FILE
-
 if [[ "$ARCH" == "x86_64" ]]; then
 	BZ_FILE="/vm/v${K_VER}.img.bz2"
 elif [[ "$ARCH" == "aarch64" ]]; then
