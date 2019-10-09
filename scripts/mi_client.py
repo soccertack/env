05052d5caac52dbd72b3c29dbef3b881aed29406
Jintack Lim <jintack@cs.columbia.edu>
2019-10-09 09:27:09 -0700

    Fix mg indentation. No functional change
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- scripts/mi_client.py
+++ scripts/mi_client.py
@@ -23,17 +23,17 @@ monitor_child = None
 dest_child = None
 
 def connect_to_server():
-	print("Trying to connect to the server")
-	clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Trying to connect to the server")
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	try:
-		clientsocket.connect(('10.10.1.1', PORT))
-	except:
-		print ('Not conncted.')
-                return None
+    try:
+        clientsocket.connect(('10.10.1.1', PORT))
+    except:
+        print ('Not conncted.')
+        return None
 
-	print("Connected")
-	return clientsocket
+    print("Connected")
+    return clientsocket
 
 migration_ret = []
 def get_migration_info(migration_output):
@@ -46,125 +46,126 @@ def get_migration_info(migration_output):
             migration_ret.append(match.groups())
 
 def handle_recv(c, buf):
-	global status
-	global dest_child
+    global status
+    global dest_child
 
-	print buf + " is received"
-	if status == C_WAIT_FOR_BOOT_CMD:
-		if buf == MSG_BOOT:
-			print ("reuse param: ", reuse_param)
-			vm_api.init(reuse_param)
-			vm_api.boot_vms()
-			c.send(MSG_BOOT_COMPLETED)
-			status = C_BOOT_COMPLETED
-	elif status == C_BOOT_COMPLETED:
-		if buf == MSG_MIGRATE:
-			global monitor_child
-			print "start migration"
-			child = vm_api.get_child()
-			mi_level = vm_api.get_mi_level()
+    print buf + " is received"
+    if status == C_WAIT_FOR_BOOT_CMD:
+        if buf == MSG_BOOT:
+            print ("reuse param: ", reuse_param)
+            vm_api.init(reuse_param)
+            vm_api.boot_vms()
+            c.send(MSG_BOOT_COMPLETED)
+            status = C_BOOT_COMPLETED
+    elif status == C_BOOT_COMPLETED:
+        if buf == MSG_MIGRATE:
+            global monitor_child
+            print "start migration"
+            child = vm_api.get_child()
+            mi_level = vm_api.get_mi_level()
 
-			monitor_child = pexpect.spawn('bash')
-			monitor_child.logfile_read=sys.stdout
-			monitor_child.timeout=None
-                        
-                        if mi_level == 2:
-		    	    monitor_child.sendline('ssh root@10.10.1.100')
-                            vm_api.wait_for_prompt(monitor_child, vm_api.hostnames[1])
+            monitor_child = pexpect.spawn('bash')
+            monitor_child.logfile_read=sys.stdout
+            monitor_child.timeout=None
 
-			monitor_child.sendline('telnet 127.0.0.1 4444')
-			monitor_child.expect('\(qemu\)')
+            if mi_level == 2:
+                monitor_child.sendline('ssh root@10.10.1.100')
+                vm_api.wait_for_prompt(monitor_child, vm_api.hostnames[1])
 
-			child = monitor_child
+            monitor_child.sendline('telnet 127.0.0.1 4444')
+            monitor_child.expect('\(qemu\)')
 
-                        #Set migration speed to max for testing
-                        if vm_api.get_mi_fast():
-                            child.sendline('migrate_set_speed 4095m')
-                            child.expect('\(qemu\)')
-			if mi_level  == 2:
-				child.sendline('migrate -d tcp:10.10.1.110:5555')
-			elif mi_level  == 1:
-				child.sendline('migrate -d tcp:10.10.1.3:5555')
-			else:
-				print("Error: mi level is " + int(mi_level))
-				sys.exit(1)
-			child.expect('\(qemu\)')
+            child = monitor_child
 
-			while True:
-				time.sleep(10)
-				child.sendline('info migrate')
-				child.expect('\(qemu\)')
-				if "Migration status: completed" in child.before:
-                                        get_migration_info(child.before)
-					break;
+            #Set migration speed to max for testing
+            if vm_api.get_mi_fast():
+                child.sendline('migrate_set_speed 4095m')
+                child.expect('\(qemu\)')
 
-			time.sleep(3)
-			print "migration completed"
-			c.send(MSG_MIGRATE_COMPLETED)
-			status = C_MIGRATION_COMPLETED
+            if mi_level  == 2:
+                child.sendline('migrate -d tcp:10.10.1.110:5555')
+            elif mi_level  == 1:
+                child.sendline('migrate -d tcp:10.10.1.3:5555')
+            else:
+                print("Error: mi level is " + int(mi_level))
+                sys.exit(1)
+            child.expect('\(qemu\)')
 
-                # This is only on the destination
-		if buf == MSG_MIGRATE_CHECK:
+            while True:
+                time.sleep(10)
+                child.sendline('info migrate')
+                child.expect('\(qemu\)')
+                if "Migration status: completed" in child.before:
+                    get_migration_info(child.before)
+                    break;
 
-			mi_level = vm_api.get_mi_level()
-			dest_child = pexpect.spawn('bash')
-			dest_child.logfile_read=sys.stdout
-			dest_child.timeout=None
-                        
-                        if mi_level == 2:
-		    	    dest_child.sendline('ssh root@10.10.1.110')
-                            vm_api.wait_for_prompt(dest_child, vm_api.hostnames[1])
-			dest_child.sendline('telnet 127.0.0.1 4445')
-                        dest_child.sendline('')
-    			dest_child.expect('L.*#')
-                        dest_child.sendline('ls')
-    			dest_child.expect('L.*#')
-                        dest_child.sendline('ls -al')
-    			dest_child.expect('L.*#')
+            time.sleep(3)
+            print "migration completed"
+            c.send(MSG_MIGRATE_COMPLETED)
+            status = C_MIGRATION_COMPLETED
 
-			c.send(MSG_MIGRATE_CHECKED)
-			status = C_MIGRATION_COMPLETED
+            # This is only on the destination
+        if buf == MSG_MIGRATE_CHECK:
+            mi_level = vm_api.get_mi_level()
+            dest_child = pexpect.spawn('bash')
+            dest_child.logfile_read=sys.stdout
+            dest_child.timeout=None
 
-	if buf == MSG_TERMINATE:
-		if monitor_child:
-			vm_api.terminate_vms(True, monitor_child)
-		elif dest_child:
-			vm_api.terminate_vms(False, dest_child)
-                else:
-			vm_api.terminate_vms(True)
-		c.send(MSG_TERMINATED)
-		status = C_TERMINATED
+            if mi_level == 2:
+                dest_child.sendline('ssh root@10.10.1.110')
+                vm_api.wait_for_prompt(dest_child, vm_api.hostnames[1])
+
+            dest_child.sendline('telnet 127.0.0.1 4445')
+            dest_child.sendline('')
+            dest_child.expect('L.*#')
+            dest_child.sendline('ls')
+            dest_child.expect('L.*#')
+            dest_child.sendline('ls -al')
+            dest_child.expect('L.*#')
+
+            c.send(MSG_MIGRATE_CHECKED)
+            status = C_MIGRATION_COMPLETED
+
+    if buf == MSG_TERMINATE:
+        if monitor_child:
+            vm_api.terminate_vms(True, monitor_child)
+        elif dest_child:
+            vm_api.terminate_vms(False, dest_child)
+        else:
+            vm_api.terminate_vms(True)
+        c.send(MSG_TERMINATED)
+        status = C_TERMINATED
 
 reuse_param = False
 def main():
-	global status
-        global reuse_param
-	
-        rerun = vm_api.get_boolean_input("Want to re-do migration automatically?[y/n]")
-        os.system("./setup-nfs-client.sh")
-	clientsocket = connect_to_server()
-        if not clientsocket:
-            sys.exit(0)
-	status = C_WAIT_FOR_BOOT_CMD
+    global status
+    global reuse_param
 
-	while True:
-		buf = clientsocket.recv(size)
-		if not buf:
-			print("Server is disconnected")
-			sys.exit(0)
-		else:
-			handle_recv(clientsocket, buf)
-			if status == C_TERMINATED:
-                                if rerun:
-                                    reuse_param = True
-                                    time.sleep(15)
-                                    print("Reconnect server")
-                                    clientsocket = connect_to_server()
-                                    if not clientsocket:
-                                        break
-                                    status = C_WAIT_FOR_BOOT_CMD
-                                else:
-                                    break
+    rerun = vm_api.get_boolean_input("Want to re-do migration automatically?[y/n]")
+    os.system("./setup-nfs-client.sh")
+    clientsocket = connect_to_server()
+    if not clientsocket:
+        sys.exit(0)
+    status = C_WAIT_FOR_BOOT_CMD
+
+    while True:
+        buf = clientsocket.recv(size)
+        if not buf:
+            print("Server is disconnected")
+            sys.exit(0)
+        else:
+            handle_recv(clientsocket, buf)
+            if status == C_TERMINATED:
+                if rerun:
+                    reuse_param = True
+                    time.sleep(15)
+                    print("Reconnect server")
+                    clientsocket = connect_to_server()
+                    if not clientsocket:
+                        break
+                    status = C_WAIT_FOR_BOOT_CMD
+                else:
+                    break
 
 if __name__ == '__main__':
-	main()
+    main()
